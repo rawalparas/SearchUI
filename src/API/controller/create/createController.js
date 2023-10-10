45ebbdc5121bc0a2cd43d3136e9cec85fbb6dffe
rawalparas <rawal.paras@athansys.com>
2023-10-10 16:42:14 +0530

    INSERTION

--- src/API/controller/create/createController.js
+++ src/API/controller/create/createController.js
@@ -2,33 +2,38 @@ const author = require("../../../model/authorModel.js");
 const messages = require("../../../helper/messages.js");
 const book = require("../../../model/booksModel.js");
 const language = require("../../../model/languageModel.js");
+const search = require('../../../model/searchModel.js');
 
 module.exports = {
   insertBooks: async (req, res) => {
     try {
-      let languageId = await language.findOne({ name : req.body.language });
+      let languageId = await language.findOne({ name: req.body.language });
       if (!languageId || languageId.length == 0) {
-        languageId = await language.create({ name : req.body.language });
+        languageId = await language.create({ name: req.body.language });
       }
 
-      let authorId = await author.findOne({ name : req.body.author });
+      let authorId = await author.findOne({ name: req.body.author });
       if (!authorId || authorId.length == 0) {
-        authorId = await author.create({ name : req.body.author });
+        authorId = await author.create({ name: req.body.author });
       }
-
       if (languageId._id && authorId._id) {
         bookData = await book.create({
           name: req.body.name,
           authorId: authorId._id,
           languageId: languageId._id,
         });
-      }else{
-        return res.send(400).send(messages.BAD_REQUEST);
+      } else if (languageId._id || authorId.id) {
+        const searchData = await search.create({
+          name: req.body.name,
+          s_id: req.body.languageId
+        })
+      } else {
+        return res.status(400).send(messages.BAD_REQUEST)
       }
       return res.status(200).send(messages.SUCCESSSFULLY_CREATED);
     } catch (err) {
       console.error(err);
       res.status(500).send(messages.INTERNAL_SERVER_ERROR);
     }
-  },
+  }
 };
---
+++ src/model/searchModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const regex = require('../helper/regexPatterns.js');
+const validateModel = require('../helper/validateModelMethod.js');
+
+const searchSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        validate : {
+            validator : function(value){
+                return validateModel.validateName(value , regex.name)
+            },
+            message : function(props){
+                return validateModel.validateMessageName(props , regex.name)
+            }
+        }
+    },
+    s_id : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        validate : {
+            validator : function(value){
+                return 
+            },
+            message : function(props){
+                return 
+            }
+        }
+    }
+})
