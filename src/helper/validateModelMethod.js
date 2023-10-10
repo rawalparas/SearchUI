43776c57273885394044d154103be23cf8b717d0
rawalparas <rawal.paras@athansys.com>
2023-10-10 16:06:56 +0530

    INSERTION

--- src/helper/validateModelMethod.js
+++ src/helper/validateModelMethod.js
@@ -1,17 +1,21 @@
+const message = require('../helper/messages.js');
+
 module.exports = {
-    validateName(regex, value) {
+    validateName(value, regex) {
+        console.log(regex)
+
         return regex.test(value);
     },
-
-    validateMessageName(regex, props) {
-        console.log(regex);
-        console.log(props);
-        // if (props.charAt(0) == ' ' || props.charAt(props.length - 1) == ' ')
-        //     return `${props.value} : ${message.NOT_CONTAIN_SPACES_AT_START_AND_END}`
-        // if (props.length < 2 || props.length > 50)
-        //     return `${props.value} : ${message.INVALID_LENGTH}`
-        // if (!regex.test(props))
-        //     return `${props} : ${message.MUST_NOT_CONTAIN_SPEC_CHAR_AND_NUM}`
-        // return message.INVALID_FORMAT;
+    validateMessageName(props, regex) {
+        if (props.value.charAt(0) === ' ' || props.value.charAt(props.length - 1) === ' ') {
+            return `${props} : ${message.NOT_CONTAIN_SPACES_AT_START_AND_END}`;
+        }
+        if (props.length < 2 || props.length > 50) {
+            return `${props} : ${message.INVALID_LENGTH}`;
+        }
+        if (!regex.test(props)) {
+            return `${props} : ${message.MUST_NOT_CONTAIN_SPEC_CHAR_AND_NUM}`;
+        }
+        return `${props} : ${message.INVALID_FORMAT}`;
     }
-}
+};
--- src/model/booksModel.js
+++ src/model/booksModel.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
-const message = require('../helper/messages.js');
 const regex = require('../helper/regexPatterns.js');
-const validateSchemaName = require('../helper/validateModelMethod.js')
+const validateSchemaName = require('../helper/validateModelMethod.js');
 
 const bookSchema = new mongoose.Schema({
     name: {
       type: String,
       required: true,
       validate : {
-        validator: validateSchemaName.validateName(regex.name , value),
-        message : validateSchemaName.validateMessageName(regex.name , value),
-      },
+        validator : function(value){
+          return validateSchemaName.validateName(value,regex.name);
+        },
+        message : function(props){
+          return validateSchemaName.validateMessageName(props.value , regex.name)
+        }
+      }
     },
     authorId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -24,18 +27,3 @@ const bookSchema = new mongoose.Schema({
   
 const model = mongoose.model("Book", bookSchema);
 module.exports = model;
-
-
-// function validateName(regex , value){
-//     return regex.test(value);
-// }
-
-// function validateMessageName(regex , props) {
-//     if (props.value.charAt(0) == ' ' || props.value.charAt(props.length - 1) == ' ')
-//         return `${props.value} : ${message.NOT_CONTAIN_SPACES_AT_START_AND_END}`
-//     if (props.value.length < 2 || props.value.length > 50)
-//         return `${props.value} : ${message.INVALID_LENGTH}`
-//     if (!regex.test(props.value))
-//         return `${props.value} : ${message.MUST_NOT_CONTAIN_SPEC_CHAR_AND_NUM}`
-//     return message.INVALID_FORMAT;
-// }
