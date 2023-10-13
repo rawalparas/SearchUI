7476281ab20269dba4b9d1d80616fefc58b67194
rawalparas <rawal.paras@athansys.com>
2023-10-13 17:57:32 +0530

    SEARCH DATA

--- src/API/controller/search/searchController.js
+++ src/API/controller/search/searchController.js
@@ -1,13 +1,16 @@
 const messages = require("../../../helper/messages.js");
-const bookModel = require('../../../model/bookModel.js');
-const searchModel = require('../../../model/searchModel.js');
+const bookModel = require("../../../model/bookModel.js");
+const searchModel = require("../../../model/searchModel.js");
 
 module.exports = {
   // method to get all the details of books.
   searchBook: async (req, res) => {
     try {
-      const getBooksData = await bookModel.find({}, { _id: 0, __v: 0 }).populate("authorId", "-_id -__v").populate("languageId", "-_id -__v");
-      return res.status(200).json({ "books": getBooksData })
+      const getBooksData = await bookModel
+        .find({}, { _id: 0, __v: 0 })
+        .populate("authorId", "-_id -__v")
+        .populate("languageId", "-_id -__v");
+      return res.status(200).json({ books: getBooksData });
     } catch (error) {
       console.log(error);
       return res.status(500).send(messages.INTERNAL_SERVER_ERROR);
@@ -16,15 +19,21 @@ module.exports = {
   // method to get the data from the search collection.
   searchData: async (req, res) => {
     try {
-      const field = req.body.field;
+      let search = req.body.search;
       const pageNumber = req.body.pageNumber;
       const limit = req.body.limit || 5;
       const offset = (pageNumber - 1) * limit;
-      const searchData = await searchModel.find({ name: field }).skip(offset).limit(limit);
+      const searchData = await searchModel
+        .aggregate([
+          { $match: { name: { $regex: search, $options: "i" } } },
+          { $project: { name: 1, s_id: 1 , _id : 0} },
+        ])
+        .skip(offset)
+        .limit(limit);
       return res.status(200).send(searchData);
     } catch (error) {
       console.log(error);
       return res.status(500).send(messages.INTERNAL_SERVER_ERROR);
     }
-  }
+  },
 };
--- src/API/middleware/joiSchema.js
+++ src/API/middleware/joiSchema.js
@@ -3,14 +3,14 @@ const message = require('../../helper/messages.js');
 const regex = require('../../helper/regexPatterns.js');
 
 module.exports = {
-    // insert : Joi.object({
-    //     name : Joi.string().trim().min(2).max(50).regex(regex.name).required(),
-    //     author : Joi.string().trim().min(2).max(50).regex(regex.author).required(),
-    //     language : Joi.string().trim().min(2).max(50).regex(regex.language).required()
-    // }),
     insert : Joi.object({
         name : Joi.string().required(),
         author : Joi.string().required(),
         language : Joi.string().required()
+    }),
+    search : Joi.object({
+        search : Joi.string().required(),
+        pageNumber : Joi.number().min(1).required(),
+        limit : Joi.number()
     })
 }
