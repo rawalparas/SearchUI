5e47721cac10d21fb98ff1e8856b36121a646f10
rawalparas <rawal.paras@athansys.com>
2023-10-13 14:57:44 +0530

    INSERTION

--- src/API/controller/search/searchRoutes.js
+++ src/API/controller/search/searchRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const searchController = require('./searchController.js'); 
 
 router.get('/searchbook' , searchController.searchBook);
+router.post('/searchdata', searchController.searchData);
 
 module.exports = router;
